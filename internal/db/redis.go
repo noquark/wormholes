1676cc2896181134ed8c6fe3e0898ed0828778a9
Mohit Singh <mohit@mohitsingh.in>
2021-09-03 15:05:32 +0530

    Switch from redigo to go-redis

--- internal/db/redis.go
+++ internal/db/redis.go
@@ -1,14 +1,20 @@
 package db
 
-import "github.com/gomodule/redigo/redis"
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/rs/zerolog/log"
+)
 
 // Config for Redis.
 type Redis struct {
-	URI string `env:"REDIS_URI" envDefault:"localhost:6379"`
+	URI string `env:"REDIS_URI" envDefault:"redis://:redis@localhost:6379/0"`
 }
 
-func (r *Redis) Connect() *redis.Pool {
-	return &redis.Pool{
-		Dial: func() (redis.Conn, error) { return redis.Dial("tcp", r.URI) },
+func (r *Redis) Connect() *redis.Client {
+	opts, err := redis.ParseURL(r.URI)
+	if err != nil {
+		log.Error().Err(err).Msg("redis: failed to parse url")
 	}
+
+	return redis.NewClient(opts)
 }
