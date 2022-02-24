f9ee8bd923f604b679b1aca3abd4a500101ba8ca
Mohit Singh <mohit@mohitsingh.in>
2022-02-24 12:08:15 +0530

    Include missing director

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"os"
 	"wormholes/services/creator"
+	"wormholes/services/director"
 	"wormholes/services/generator"
 
 	"github.com/rs/zerolog"
@@ -26,11 +27,10 @@ func main() {
 	switch *as {
 	case Generator:
 		generator.Run()
-
 	case Creator:
 		creator.Run()
 	case Director:
-
+		director.Run()
 	default:
 		log.Error().Msg("Available options are generator, creator and director")
 	}
