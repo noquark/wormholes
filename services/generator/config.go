ea93dd448fd7eeddcf320038fc7333fea7e65d69
singhisbling <nitesh2004singh@gmail.com>
2023-03-11 10:11:16 +0530

    refactor generator module

--- services/generator/config.go
+++ services/generator/config.go
@@ -1,17 +1,20 @@
 package generator
 
 import (
+	"time"
+
 	"github.com/caarlos0/env/v6"
 	"github.com/rs/zerolog/log"
 )
 
 type Config struct {
-	Port           int     `env:"PORT" envDefault:"5001"`
-	IDSize         int     `env:"ID_SIZE" envDefault:"7"`
-	BucketSize     int     `env:"BUCKET_SIZE" envDefault:"16"`
-	BucketCapacity int     `env:"BUCKET_CAP" envDefault:"100000"`
-	BloomMaxLimit  uint    `env:"BLOOM_MAX" envDefault:"100000000"`
-	BloomErrorRate float64 `env:"BLOOM_ERROR" envDefault:"0.0000001"`
+	Port           int           `env:"PORT" envDefault:"5001"`
+	IDSize         int           `env:"ID_SIZE" envDefault:"7"`
+	BucketSize     int           `env:"BUCKET_SIZE" envDefault:"16"`
+	BucketCapacity int           `env:"BUCKET_CAP" envDefault:"100000"`
+	BloomMaxLimit  uint          `env:"BLOOM_MAX" envDefault:"100000000"`
+	BloomErrorRate float64       `env:"BLOOM_ERROR" envDefault:"0.0000001"`
+	Timeout        time.Duration `env:"TIMEOUT" envDefault:"100ms"`
 }
 
 func DefaultConfig() *Config {
